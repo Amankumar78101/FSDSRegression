4570c2330a0f706c2526f3072417d30a035c7c83
Aman kumar <atulkumar427810@gmail.com>
2023-04-02 17:03:21 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup ,find_packages
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -6,21 +6,20 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-       requirements=file_obj.readline()
-       requirements=[req.replace('\n','') for req in requirements]
-       if HYPEN_E_DOT in requirements :
-           requirements.remove(HYPEN_E_DOT)
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
     return requirements
- 
-    
 
 
-
-setup(name="Regressorproject",
-      version="0.0.1", 
-      author='aman',
-      author_email="amankumar@gmail.com",
-      install_requires=get_requirements('requirements.txt'),
-      packages=find_packages()
-
-      )
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    author='Aman',
+    author_email='amankumar@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
